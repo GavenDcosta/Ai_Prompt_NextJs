5e2eaea136aa5f9215c1ace9bcebf6ee07a61373
Gaven Dcosta <gavendcosta@gmail.com>
2023-09-22 11:09:09 +0530

    create-post started

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+import Form from '@components/Form'
 
 const CreatePrompt = () => {
+
+  const [submittng, setSubmittng] = useState(false)
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: ""
+  })
+
+  const createPrompt = async (e) => {
+
+  }
+
+
   return (
-    <div>CreatePrompt</div>
+    <Form 
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submittng}
+      handleSubmit={createPrompt}
+    />
   )
 }
 
