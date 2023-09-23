67850a63a40e691b6dd1640c1c1288c7131945b2
Gaven Dcosta <gavendcosta@gmail.com>
2023-09-23 16:05:21 +0530

    Completed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,10 @@ import { usePathname, useRouter } from 'next/navigation'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
+  const { data:session } = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
+
   const [copied, setCopied] = useState("")
 
   const handleCopy = () => {
@@ -17,7 +21,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
   return (
     <div className='prompt_card'>
-      <div className='flex justify-between items-start gap-r'>
+      <div className='flex justify-between items-start gap-5'>
         <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
           <Image 
             src={post.creator.image}
@@ -50,8 +54,25 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <p className='font-inter text-sm blue_gradiant cursor_pointer'
        onClick={() => handleTagClick && handleTagClick}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && 
+        pathName === '/profile' && (
+          <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+            <p className='font-inter text-sm green_gradient cursor-pointer'
+              onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p className='font-inter text-sm orange_gradient cursor-pointer'
+              onClick={handleDelete}
+            >
+              Delete
+            </p>
+          </div>
+        )
+      }
     </div>
   )
 }
